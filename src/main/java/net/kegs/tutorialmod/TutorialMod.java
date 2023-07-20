e21d9a22fa7669a4a1b90e21359773315ad68358
_kegs <kieran_geraghty@hotmail.com>
2023-07-20 19:21:24 +1000

    Add sapphire item and texture

--- src/main/java/net/kegs/tutorialmod/TutorialMod.java
+++ src/main/java/net/kegs/tutorialmod/TutorialMod.java
@@ -1,7 +1,9 @@
 package net.kegs.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.kegs.tutorialmod.item.ModItems;
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -17,16 +19,17 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ToutorialMod.MOD_ID)
-public class ToutorialMod {
+@Mod(TutorialMod.MOD_ID)
+public class TutorialMod {
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "tutorial_mod";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public ToutorialMod() {
+    public TutorialMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModItems.register(modEventBus);
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
@@ -42,7 +45,9 @@ public class ToutorialMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.SAPPHIRE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
